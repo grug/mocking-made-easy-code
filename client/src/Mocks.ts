9cba551281d994b254a3576054877d4ab38eafb7
Dave Cooper <dave.cooper@ovoenergy.com>
2018-12-03 17:52:54 +0000

    wip

--- client/src/Mocks.ts
+++ client/src/Mocks.ts
@@ -2,18 +2,25 @@ import { Scenarios } from 'data-mocks';
 import { Shift } from './Entities';
 
 export const mocks: Scenarios = {
-  default: [
+  'default': [
     {
-      url: /next-shift$/,
+      url: /next-shift/,
       method: 'GET',
       response: { shiftSize: 'Large' } as Shift,
       responseCode: 200,
       delay: 250
+    },
+    {
+      url: /foo/,
+      method: 'GET',
+      response: {},
+      responseCode: 200,
+      delay: 250
     }
   ],
-  medium: [
+  'medium': [
     {
-      url: /next-shift$/,
+      url: /next-shift/,
       method: 'GET',
       response: { foo: 'bar' },
       responseCode: 200,
